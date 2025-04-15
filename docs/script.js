55e9620e96f688bbf5c1aa096156cfff71128c0a
shelly5353 <shelly5353@gmail.com>
2025-04-15 14:56:03 +0300

    Update Google Apps Script deployment URL and CORS settings

--- docs/script.js
+++ docs/script.js
@@ -233,7 +233,7 @@ document.addEventListener('DOMContentLoaded', function() {
             };
             
             try {
-                const response = await fetch('https://script.google.com/macros/s/AKfycbyr9u1-oBEJsJ35uWueewykjUp68acbFhpqL1sM9jXn6RaTj6gimSqV-ta7ZRNt_is/exec', {
+                const response = await fetch('https://script.google.com/macros/s/AKfycbxBv2L5yUGE3YPv_ggM-lunam7MQeaNDw0WUbjEZDh8tK5YLhHSctPcJuouLBT37FY/exec', {
                     method: 'POST',
                     mode: 'cors',
                     cache: 'no-cache',
